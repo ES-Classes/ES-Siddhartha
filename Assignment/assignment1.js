ad181c9f5b59ca80214066f23ac208e34ea0aa38
Siddhartha <Siddhartharaut33@gmail.com>
2022-04-10 17:29:38 +0545

    assignment-1

---
+++ Assignment/assignment1.js
@@ -0,0 +1,12 @@
+let pm=45;
+let ph=1.8;
+let rm=92;
+let rh=1;
+
+const BMIp=pm/(ph*ph);
+const BMIr=rm/(rh*rh);
+console.log(BMIp);
+console.log(BMIr);
+
+let prabinHigherBmi= BMIp>BMIr === true;
+console.log(prabinHigherBmi);
